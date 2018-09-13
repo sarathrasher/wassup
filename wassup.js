b8c01a706179f91d706a868e70dfb4ff102120de
Sara Muntean <munteansr@gmail.com>
2018-09-13 14:35:55 -0400

    Added functionality to grab data from form and update page accordingly

--- wassup.js
+++ wassup.js
@@ -1,46 +1,78 @@
 const h = React.createElement;
 
-let createRandomId = () =>
-  Math.floor(Math.random * Math.floor(1000));
+let generateId = () =>
+  Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
 
 const wassups = [
   { date: new Date(),
     content: "Blargh Blargh Blargh",
-    user: 'Pteraducktal',
+    user: 'Ptera🦆tal',
     id: 1,
   },
   { date: new Date(),
     content: "It's hard to know how to React to this",
-    user: 'Tyranasaurus Duck',
+    user: 'Tyranasaurus 🐤',
     id: 2,
   },
   { date: new Date(),
-    content: "Re🐣🐥",
-    user: 'Velociducktor',
+    content: "Re🦆🦆",
+    user: 'Veloci🐥tor',
     id: 3,
   },
 ]
 
-let WassupForm = (props) =>
-  h('form', { className: 'wassup-form' }, [
-    h('textarea', { className: 'wassup-input', placeholder: 'Wassup?' }),
-    h('input', { className: 'username-input', placeholder: 'Username' }),
-    h('button', 
-      { className: 'submit-button', type: 'submit',
-        onClick: (event) => {
-          event.preventDefault();
-          return h(WassupRow, { /*Input Value*/ })
+class WassupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newWassupValue: '',
+      newUserValue: '',
+    }
+  }
+  render() {
+    return h('form', { 
+      className: 'wassup-form',
+      onSubmit: (event) => {
+        event.preventDefault();
+        this.props.addWassup(this.state.newWassupValue, this.state.newUserValue)
+      }
+      }, 
+      h('textarea', { 
+        className: 'wassup-input', 
+        placeholder: 'Wassup?', 
+        type: "text",
+        value: this.state.newWassupValue,
+        onChange: (event) => {
+          this.setState({
+            newWassupValue: event.target.value
+        })
+      }
+      }),
+      h('input', { 
+        className: 'username-input', 
+        placeholder: 'Username', 
+        type: "text",
+        value: this.state.newUserValue, 
+        onChange: (event) => {
+          this.setState({
+            newUserValue: event.target.value
+          })
         }
-      }, ['Post'])
-    ]
-  );
+      }),
+      h('button', 
+        { className: 'submit-button', type: 'submit',
+        }, 'Post')
+      );
+    }  
+  }
+  
 
 let WassupRow = (props) => 
-  h('li', {className: 'wassup-row' }, [
+  h('li', {className: 'wassup-row' }, 
     h('h4', { className: 'wassup-content' }, props.wassup.content),
     h('p', { className: 'wassup-user' }, `Posted by: ${props.wassup.user}`),
     h('p', { className: 'wassup-date' }, `Posted on: ${props.wassup.date.toString()}`),
-  ])
+  )
 
 let WassupList = (props) =>
   h('ul', 
@@ -53,15 +85,27 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      wassups: wassups
+      wassups: wassups,
     }
   }
   render() {
-    return h('div', {className: 'main' }, [
-      h('h1', { className: 'header' }, ['Wassup!']),
-      h(WassupForm),
+    let addWassup = (newWassupContent, newWassupUser) => {
+      this.setState({
+        wassups: this.state.wassups.concat([
+          {
+            id: generateId(),
+            user: newWassupUser,
+            content: newWassupContent,
+            date: new Date(),
+          }
+        ])
+      })
+    }
+    return h('div', {className: 'main' }, 
+      h('h1', { className: 'header' }, 'Wassup!'),
+      h(WassupForm, { addWassup: addWassup }),
       h(WassupList, { wassups: this.state.wassups })
-    ])
+    )
   }
 }
 
